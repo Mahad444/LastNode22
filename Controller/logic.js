fb73eb7277e18f09a578c1bc1bb5928e64644dc1
Mahad444 <mahadsaid95@icloud.com>
2022-12-24 10:57:50 +0300

    User

---
+++ Controller/logic.js
@@ -0,0 +1,10 @@
+const user = require('../Model/user')
+require('dotenv').config();
+
+module.exports = {
+    register:async(req,res)=>{
+        user.create()
+        res.status(200)
+            "Added successfully"
+    }
+}
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
 const app = express();
+const router = require('./Controller/api')
+app.use(router);
 app.use(cors());
 
-app.listen(process.env.port || 6600 ,()=>{
-    console.log("application running http://loaclhost/6600")
+app.listen(5500 ,()=>{
+    console.log("application running http://loaclhost/5500/")
 } )
 
 mongoose.connect('mongodb://localhost:27017/LoanDB');
